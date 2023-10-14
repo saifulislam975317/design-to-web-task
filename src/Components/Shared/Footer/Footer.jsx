9a2a270c164a5f2f11572e3d9330a120f63a2c2b
saiful islam <saifulislam975317@gmail.com>
2023-10-14 12:35:52 +0600

    update info

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,8 +1,44 @@
+import footerLogo from "../../../assets/software.png";
 const Footer = () => {
   return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
+    <footer className="footer p-10 text-white footerBg">
+      <aside>
+        <img className="w-[70px] h-[70px]" src={footerLogo} alt="" />
+        <p>
+          India's first platform dedicated to <br /> simplifying partner search
+        </p>
+      </aside>
+      <nav>
+        <header className="text-[14px] font-bold">COMPANY</header>
+        <a className="link link-hover">About</a>
+        <a className="link link-hover">Pricing</a>
+        <a className="link link-hover">Careers</a>
+      </nav>
+      <nav>
+        <header className="text-[14px] font-bold">SOLUTIONS</header>
+        <a className="link link-hover">Search</a>
+        <a className="link link-hover">Connect</a>
+        <a className="link link-hover">Research</a>
+        <a className="link link-hover">Academy</a>
+      </nav>
+      <nav>
+        <header className="text-[14px] font-bold">RESOURCES</header>
+        <a className="link link-hover">Blogs</a>
+        <a className="link link-hover">Forms</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+      <nav>
+        <header className="text-[14px] font-bold">SUPPORT</header>
+        <a className="link link-hover">Help</a>
+        <a className="link link-hover">Contact Us</a>
+      </nav>
+      <nav>
+        <header className="text-[14px] font-bold"> LEGAL</header>
+        <a className="link link-hover">Privacy</a>
+        <a className="link link-hover">Terms</a>
+        <a className="link link-hover">Accessibility</a>
+      </nav>
+    </footer>
   );
 };
 
